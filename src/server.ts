d0d8c63820d9cd3080ddd5b38db3daeb3f37d366
Takmim00 <takmimm@gmail.com>
2026-08-15 21:11:04 +0600

    Fix Express 5 CORS wildcard crash

--- src/server.ts
+++ src/server.ts
@@ -69,7 +69,6 @@ const corsOptions: cors.CorsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use('/api', apiLimiter);
